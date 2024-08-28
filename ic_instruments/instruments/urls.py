90e07a1d7c89c3a40c8101c201676d3f06db9f82
Mecatronico26 <45455804+Mecatronico26@users.noreply.github.com>
2024-08-28 15:30:52 -0300

    Delete ic_instruments directory

--- ic_instruments/instruments/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import instrument_list
-
-urlpatterns = [
-    path('', instrument_list, name='instrument_list'),  # Aquí debe estar vacío para que se mapee a /
-]
--- ic_instruments/instruments/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Instrument
-
-def instrument_list(request):
-    instruments = Instrument.objects.all()  # Obtiene todos los instrumentos
-    return render(request, 'instruments/test_copy.html', {'instruments': instruments})
