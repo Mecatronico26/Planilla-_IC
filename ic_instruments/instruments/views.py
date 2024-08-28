89aed1f8422f8e9c78712711d8f609c8bd8cfe3a
Mecatronico26 <mecatronico26.eg@gmail.com>
2024-08-28 11:12:49 -0300

    cabecera v1.1

--- ic_instruments/instruments/views.py
+++ ic_instruments/instruments/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Instrument
 
 def instrument_list(request):
-    instruments = Instrument.objects.all()
-    return render(request, 'instruments/test.html', {'instruments': instruments})
+    instruments = Instrument.objects.all()  # Obtiene todos los instrumentos
+    return render(request, 'instruments/test_copy.html', {'instruments': instruments})
